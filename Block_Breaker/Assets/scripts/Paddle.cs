e01ff1e6fb6bc2f486ed7f8d4da971cd0e8b939d
shannongusman <shannongusman@users.noreply.github.com>
2019-10-16 11:23:56 +0200

    lesson 3
    switching scene on collision
    mouse position

--- Block_Breaker/Assets/scripts/Paddle.cs
+++ Block_Breaker/Assets/scripts/Paddle.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Paddle : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      print(Input.mousePosition.x / Screen.width*16f);
     }
 }
