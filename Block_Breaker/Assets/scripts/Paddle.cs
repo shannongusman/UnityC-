7a694996cef169c8942ebb9a3a4c1c9f5b0ddde4
shannongusman <shannongusman@users.noreply.github.com>
2019-10-21 16:10:30 +0200

    ballPosition
    ball distance and paddle

--- Block_Breaker/Assets/scripts/Paddle.cs
+++ Block_Breaker/Assets/scripts/Paddle.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    Vector2 paddlePosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    paddlePosition = new Vector2 (transform.position.x,transform.position.y);        
     }
 
     // Update is called once per frame
     void Update()
     {
-      print(Input.mousePosition.x / Screen.width*16f);
+      float mousePos = Input.mousePosition.x / Screen.width*screenWidthInUnits;
+
+      //setting the limit of the mouse inthe x-axis between 1 and 15 
+
+       paddlePosition.x = Mathf.Clamp(mousePos , 1f, 15f );
+
+      this.transform.position = paddlePosition;
+
+
     }
 }
---
+++ Block_Breaker/Assets/scripts/ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    [SerializeField]Paddle myPaddle;
+    Vector2 paddleToBallDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallDistance = this.transform.position - myPaddle.transform.position;
+
+        //ball - paddlePosition
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePosition = myPaddle.transform.position;
+
+        //set the ball position = paddle position +
+        this.transform.position = paddlePosition + paddleToBallDistance;
+        
+    }
+}
