745eb5b9993884b12830c9af085526ac6f8ed60a
shannongusman <shannongusman@users.noreply.github.com>
2019-10-28 16:14:56 +0100

    start and quit
    start and quit button
    sceneLoader

---
+++ Block_Breaker/Assets/scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
